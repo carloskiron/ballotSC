e6e1a3adea7ebbdef530e42e187273eba7e7a17f
Carlos Montoya <carlos@teamclass.com>
2022-10-01 22:40:21 -0500

    scripts

---
+++ scripts/util/scriptUtils.ts
@@ -0,0 +1,11 @@
+import { ethers } from "hardhat";
+
+function convertStringArrayToBytes32(array: string[]) {
+    const bytes32Array = [];
+    for (let index = 0; index < array.length; index++) {
+      bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+    }
+    return bytes32Array;
+  }
+
+export { convertStringArrayToBytes32 };  
